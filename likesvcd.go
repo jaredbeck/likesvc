0f5f0c50a64a03b2d0763a43fcd13cc3100e703b
Jared Beck <jared@jaredbeck.com>
2013-11-30 19:50:35 -0500

    gets strict about integer ids

--- likesvcd.go
+++ likesvcd.go
@@ -4,10 +4,11 @@ import (
   "fmt"
   "net/http"
   "net/url"
+  "strconv"
   "strings"
 )
 
-var likes map[string]int
+var likes map[int64]int64
 
 /*
 `LikeHandler` implements `http.Handler`
@@ -24,8 +25,12 @@ reference" (with a `*`) or "by value".
 - http://tour.golang.org/#52
 */
 func (l LikeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  var id = LastPathPart(r.URL)
-  likes[id] ++
+  var id, err = strconv.ParseInt(LastPathPart(r.URL), 10, 64)
+  if err == nil {
+    likes[id] ++
+  } else {
+    fmt.Fprintf(w, "%v\n", err)
+  }
 
   /* when printing structs, the plus flag (%+v) adds field names
   http://golang.org/pkg/fmt */
@@ -38,7 +43,7 @@ func LastPathPart(u *url.URL) string {
 }
 
 func main() {
-  likes = make(map[string]int)
+  likes = make(map[int64]int64)
   var lh LikeHandler
   http.Handle("/like/", lh)
 	http.ListenAndServe("localhost:4000", nil)
